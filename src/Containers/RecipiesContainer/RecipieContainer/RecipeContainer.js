9edfaf54ae3c1ebce86514df05c5c2a360a4f82b
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-03-30 21:48:23 +0200

    continue with containers

--- src/Containers/RecipiesContainer/RecipieContainer/RecipeContainer.js
+++ src/Containers/RecipiesContainer/RecipieContainer/RecipeContainer.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { useRouteMatch } from "react-router-dom";
-import Card from "@material-ui/core/Card";
+import { Grid, Card, CardHeader } from "@material-ui/core";
+import { recipies } from "../../../data";
+import styled from "styled-components";
+
+const StyledCard = styled(Card)`
+  width: 100%;
+`;
 
 const RecipieContainer = () => {
   const match = useRouteMatch();
-  console.log(match);
-  return <Card>{match.params.name}</Card>;
+  const recipie = recipies.find(recipie => (recipie.link = match.params.name));
+  return (
+    <Grid container>
+      <Grid item sx={6}>
+        <StyledCard>
+          <CardHeader title="Ingredienser" />
+        </StyledCard>
+      </Grid>
+      <Grid item sx={6}>
+        <StyledCard>
+          <CardHeader title="Fremgangsmetode" />
+        </StyledCard>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default RecipieContainer;
--- src/data.js
+++ src/data.js
@@ -1,10 +1,24 @@
 export const recipies = [
   {
+    id: 1,
     title: "Livens havrekjeks",
     timeMinutes: 0,
     timeHours: 3,
     dateCreated: "Mars 2020",
     link: "havrekjeks",
-    isLocal: true
+    isLocal: true,
+    ingredients: [
+      { name: "havrgryn", amount: 300, metric: "g" },
+      { name: "melk", amount: 5, metric: "dl" },
+      { name: "vann", amount: 2, metric: "dl" },
+      { name: "smør", amount: 50, metric: "g" }
+    ],
+    steps: [
+      "Smelt smør",
+      "Bland ut alt det tørre, ha i melk og vann og la det ligge i 20 min",
+      "Skru på ovnen på 200 grader",
+      "Kvelve ut deigen i 10 cm kvadrater",
+      "Smell i ovenen i 20 min"
+    ]
   }
 ];
