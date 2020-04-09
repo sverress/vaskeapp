53ea24f67a61e69841076f2bd010a391fd053799
Sverre Stray Spetalen <sverress@stud.ntnu.no>
2020-04-09 12:43:28 +0200

    small changes

--- src/Containers/RecipiesContainer/RecipieContainer/RecipeContainer.js
+++ src/Containers/RecipiesContainer/RecipieContainer/RecipeContainer.js
@@ -4,23 +4,35 @@ import { Grid, Card, CardHeader } from "@material-ui/core";
 import { recipies } from "../../../data";
 import styled from "styled-components";
 
-const StyledCard = styled(Card)`
-  width: 100%;
+const StyledCard = styled.div`
+  background-color: #f5f5f5;
 `;
 
 const RecipieContainer = () => {
   const match = useRouteMatch();
   const recipie = recipies.find(recipie => (recipie.link = match.params.name));
   return (
-    <Grid container>
+    <Grid container sx={12}>
       <Grid item sx={6}>
         <StyledCard>
-          <CardHeader title="Ingredienser" />
+          <ul>
+            {recipie.ingredients.map(ingredient => (
+              <li>
+                {ingredient.name}
+                {ingredient.amount}
+                {ingredient.metric}
+              </li>
+            ))}
+          </ul>
         </StyledCard>
       </Grid>
       <Grid item sx={6}>
         <StyledCard>
-          <CardHeader title="Fremgangsmetode" />
+          <ol>
+            {recipie.steps.map(step => (
+              <li>{step}</li>
+            ))}
+          </ol>
         </StyledCard>
       </Grid>
     </Grid>
